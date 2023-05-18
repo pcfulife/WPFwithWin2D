8375cec8e050b2e6866334d3836031cde01e2b7a
pcfulife <pcfulife@gmail.com>
2023-05-18 15:47:39 +0900

    Stuttering test

--- WPFwithWin2D/MainWindow.xaml.cs
+++ WPFwithWin2D/MainWindow.xaml.cs
@@ -1,14 +1,11 @@
 ﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Input;
 
 namespace WPFwithWin2D
 {
     public partial class MainWindow : INotifyPropertyChanged
     {
-        public Visibility WPFGraphicsVisibility { get; set; } = Visibility.Visible;
-
-        public Visibility UWPGraphicsVisibility { get; set; } = Visibility.Collapsed;
+        public Visibility UWPGraphicsVisibility { get; set; } = Visibility.Visible;
 
         public MainWindow()
         {
@@ -16,14 +13,5 @@ namespace WPFwithWin2D
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-
-        void Window_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            WPFGraphicsVisibility = WPFGraphicsVisibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
-            PropertyChanged(this, new PropertyChangedEventArgs(nameof(WPFGraphicsVisibility)));
-
-            UWPGraphicsVisibility = UWPGraphicsVisibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
-            PropertyChanged(this, new PropertyChangedEventArgs(nameof(UWPGraphicsVisibility)));
-        }
     }
 }
--- WPFwithWin2D/UWPGraphics.xaml.cs
+++ WPFwithWin2D/UWPGraphics.xaml.cs
@@ -2,7 +2,12 @@
 using Microsoft.Graphics.Canvas.UI.Xaml;
 using Microsoft.Toolkit.Wpf.UI.XamlHost;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.UI;
@@ -18,10 +23,16 @@ namespace WPFwithWin2D
             InitializeComponent();
         }
 
+        [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+        public static extern uint timeBeginPeriod(uint uMilliseconds);
+
+        [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+        public static extern uint timeEndPeriod(uint uMilliseconds);
+
         void WindowsXamlHost_ChildChanged(object sender, EventArgs e)
         {
-            var viewbox = (sender as WindowsXamlHost).Child as Viewbox;
-            if (viewbox == null)
+            timeBeginPeriod(1);
+            if (!((sender as WindowsXamlHost).Child is Viewbox viewbox))
             {
                 return;
             }
@@ -36,6 +47,11 @@ namespace WPFwithWin2D
             viewbox.Child = canvasSwapChainPanel;
             Task.Run(() =>
             {
+                Thread.CurrentThread.Priority = ThreadPriority.Highest;
+                var totalFrame = 0;
+                var lastElapsedMilliseconds = 0.0;
+                var frametimes = new List<double>();
+                var stopwatch = Stopwatch.StartNew();
                 while (true)
                 {
                     using (var canvasDrawingSession = canvasSwapChain.CreateDrawingSession(Colors.White))
@@ -45,7 +61,20 @@ namespace WPFwithWin2D
 Graphics with
 Xaml Islands", new Vector2(50F, 50F), Colors.White);
                     }
-                   canvasSwapChain.Present();
+                    canvasSwapChain.Present(1);
+                    ++totalFrame;
+                    var elapsedMilliseconds = 1000.0 * stopwatch.ElapsedTicks / Stopwatch.Frequency;
+                    frametimes.Add(elapsedMilliseconds - lastElapsedMilliseconds);
+                    lastElapsedMilliseconds = elapsedMilliseconds;
+                    if (elapsedMilliseconds >= 1000)
+                    {
+                        Console.WriteLine("fps: " + totalFrame * 1000.0 / elapsedMilliseconds);
+                        Console.WriteLine("Max: " + 1000.0 / frametimes.Max());
+                        lastElapsedMilliseconds = 0.0;
+                        totalFrame = 0;
+                        frametimes.Clear();
+                        stopwatch.Restart();
+                    }
                 }
             });
         }
